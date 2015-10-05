be6d12a55da399ccbcd5ea66c17f1cdb2e68ac1d
Anthony Ellis <ajellis1989@gmail.com>
2015-10-05 15:05:09 -0400

    Adds Course model as well as seeds containing course names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005184618) do
+ActiveRecord::Schema.define(version: 20151005185340) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.integer  "course_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+
+Course.create([{name: 'Entrees'}, {name: 'Desserts'}, {name: 'Drinks'}, {name: 'Salads'}, {name: 'Appetizers'}])
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
